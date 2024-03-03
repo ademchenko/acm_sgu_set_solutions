12e233ba87894041fb5548d082fae010386845dc
Alexander Demchenko <alexdemche@gmail.com>
2024-03-03 23:59:02 +0100

    102 Coprimes
    - Accepted (with implementation of gcd)

--- 102_Coprimes/102_Coprimes.cpp
+++ 102_Coprimes/102_Coprimes.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
-#include <numeric>
+
+//gcd is defined as in Knuth "The Art of Computer Programming" volume 2.
+//The algorithm itself is the one defined by Knuth as "Modern Euclidean algorithm":
+int gcd(int u, int v)
+{
+	if (v > u)
+		std::swap(u, v);
+	
+	if (v == 0)
+		return u;
+
+	if (v < 0)
+	{
+		v = -v;
+		if (u < 0)
+			u = -u;
+	}
+
+	int r;
+	while ((r = u % v) > 0)
+	{
+		u = v;
+		v = r;
+	}
+
+	return v;
+}
 
 int main()
 {
@@ -16,9 +42,9 @@ int main()
 
 	for (int i = 1; i < N; ++i)
 	{
-		if (std::gcd(N, i) == 1)
+		if (gcd(N, i) == 1)
 			++counter;			
 	}
-
+	
 	std::cout << counter;
 }
